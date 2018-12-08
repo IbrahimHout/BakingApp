6e97516844c9679e0f18a2ae14d039e320710181
ibrahimelhout <Ibrahimhout.dev@gmail.com>
2018-12-08 21:02:21 +0200

    edit

--- app/src/androidTest/java/com/example/ibrahimelhout/bakingapp_project4/RecipesActivityTest.java
+++ app/src/androidTest/java/com/example/ibrahimelhout/bakingapp_project4/RecipesActivityTest.java
@@ -2,9 +2,9 @@ package com.example.ibrahimelhout.bakingapp_project4;
 
 
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -17,13 +17,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -34,73 +31,22 @@ public class RecipesActivityTest {
 
     @Test
     public void recipesActivityTest() {
+
+        try {
+            //Delay to have list available for test
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         ViewInteraction linearLayout = onView(
                 allOf(withId(R.id.layout),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(R.id.foodRecycler2),
+                                        withId(R.id.foodRecycler),
                                         0),
                                 0),
                         isDisplayed()));
-        linearLayout.perform(click());
-
-        ViewInteraction appCompatTextView = onView(
-                allOf(withId(R.id.id_text), withText("0 - Recipe Introduction"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.recyclerSteps),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatTextView.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.nextButton), withText("Next Step"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        1),
-                                2),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-
-        ViewInteraction appCompatButton2 = onView(
-                allOf(withId(R.id.nextButton), withText("Next Step"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        1),
-                                2),
-                        isDisplayed()));
-        appCompatButton2.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.buttonIngredient), withText("Ingredients"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.frameMaster),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton3.perform(click());
+        linearLayout.check(matches(isDisplayed()));
 
     }
 
